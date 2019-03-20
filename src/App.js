72536042c726ee0ab72cbef39e3eb7400fd848ba
zyqq <252545095@qq.com>
2019-03-20 13:01:56 +0800

    feat: 领养页面优化

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import React, {
 import Web3 from 'web3'
 import TruffleContract from 'truffle-contract'
 import AdoptionJson from './truffle/build/contracts/Adoption.json'
+import pets from './truffle/src/pets.json'
+import { Button, Layout, Row, Col } from 'antd';
+import './App.css'
+
+const {
+  Header, Footer, Content,
+} = Layout;
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.web3 = null
     this.Adoption = null
+    this.initAddress = '0x' + '0'.repeat(40)
     this.init()
+    this.state = {
+      adopters: []
+    }
   }
   init() {
     if (typeof window.web3 !== 'undefined') {
@@ -22,7 +33,6 @@ class App extends Component {
     this.initAdoption()
   }
   initAdoption() {
-    console.log(AdoptionJson)
     this.Adoption = TruffleContract(AdoptionJson)
     this.Adoption.setProvider(this.web3Provider)
     return this.markAdopted()
@@ -30,6 +40,9 @@ class App extends Component {
   async markAdopted() {
     const adoptionInstance = await this.Adoption.deployed()
     const adopters = await adoptionInstance.getAdopters.call()
+    this.setState({
+      adopters
+    })
     console.log(adopters)
   }
   async adopt(petId) {
@@ -39,9 +52,39 @@ class App extends Component {
     await adoptionInstance.adopt(petId, { from: account })
     return this.markAdopted()
   }
+  isActive(i) {
+    return this.state.adopters[i] === this.initAddress
+  }
   render(){
     return ( 
-      <button onClick={() => this.adopt(2)}>领养第二个</button>
+      <Layout className="layout">
+        <Header>
+        </Header>
+        <Content style={{ padding: '20px 50px' }}>
+          <Row gutter={40}>
+          {
+            pets.map((v, i) => {
+              return (
+                <Col span={6} key={i}>
+                  <img src={v.picture} alt='' />
+                  <div className='center'>
+                    <p>{v.name}</p>
+                    { this.isActive(i)
+                        ? <Button type='primary' onClick={() => this.adopt(i)} >领养</Button>
+                        : <Button disabled>已被领养</Button>
+                    }
+                  </div>
+                </Col>
+              )
+            })
+          }
+          </Row>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Pet Shop ©2019 Created by Echo
+        </Footer>
+      </Layout>
+      // <Button type='primary' onClick={() => this.adopt(2)}>领养第二个</Button>
     );
   }
 }
